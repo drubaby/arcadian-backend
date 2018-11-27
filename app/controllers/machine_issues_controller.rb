1b343b3560436f4227dab8008df54baaceba09e0
drubaby <peryton6@gmail.com>
2018-11-27 14:27:31 -0500

    add controller actions for machine issues

--- app/controllers/machine_issues_controller.rb
+++ app/controllers/machine_issues_controller.rb
@@ -1,16 +1,34 @@
 class MachineIssuesController < ApplicationController
   def create
+    new_issue = LocationMachine.create(mac_issue_params)
+    render json: mac_issue.to_json
   end
 
   def index
+    render json: MachineIssue.all.to_json
   end
 
   def show
+    render json: MachineIssue.find(params[:id]).to_json
   end
 
   def update
+    mac_issue = MachineIssue.find(params[:id]).to_json
+    mac_issue.update(mac_issue_params)
+    #not sure about this
+    render json: mac_issue.to_json
   end
 
   def delete
+    mac_issue = MachineIssue.find(params[:id])
+    mac_issue.destroy()
+    render mac_issue.to_json
   end
+
+  private
+  def mac_issue_params
+    params.require(:machine_issue).permit(:location_machine_id, :description, :resolved)
+  end
+
+
 end
