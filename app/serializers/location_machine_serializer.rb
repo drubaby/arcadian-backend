34cb31cb61c8da7b43ffb573c753f7ed920c4a44
drubaby <peryton6@gmail.com>
2018-11-28 14:10:28 -0500

    add loc_machine_id to serializer

--- app/serializers/location_machine_serializer.rb
+++ app/serializers/location_machine_serializer.rb
@@ -1,5 +1,5 @@
 class LocationMachineSerializer < ActiveModel::Serializer
-  attributes :location_id, :machine, :machine_issues, :is_working
+  attributes :id, :location_id, :machine, :machine_issues, :is_working
   has_one :machine
   has_many :machine_issues
 end
