ec93f9c3156e9783cca6208f1dc8677335842cb3
drubaby <peryton6@gmail.com>
2018-11-30 18:24:54 -0500

    working on toggling loc_mac working status

--- app/controllers/location_machines_controller.rb
+++ app/controllers/location_machines_controller.rb
@@ -23,6 +23,16 @@ class LocationMachinesController < ApplicationController
     render json: new_location_machine.to_json
   end
 
+  def update
+    loc_mac = LocationMachine.find(params[:id])
+    # params is a big messy object of all location machines at this location.
+    #where does that come from?
+    # how can we get just the loc_mac i want with the switched attr
+    loc_mac.update(loc_mac_params)
+    debugger
+    render json: LocationMachine.find(params[:id])
+  end
+
   def delete
     location_machine = LocationMachine.find(params[:id])
     location_machine.destroy()
@@ -30,8 +40,8 @@ class LocationMachinesController < ApplicationController
   end
 
   private
-  def loc_mac_params
-    params.require(:location_machine).permit(:location_id, :machine_id)
-  end
 
+  def loc_mac_params
+    params.require(:location_machine).permit(:id, :location_id, :machine_id, :is_working)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :machine_issues, only: [:index, :create, :show, :update, :destroy]
-  resources :location_machines, only: [:index, :show, :create, :destroy]
+  resources :location_machines, only: [:index, :show, :create, :update, :destroy]
   resources :machines, only: [:index, :show]
   resources :locations, only: [:index, :show]
   get "/machines_at_location/:id", to: "location_machines#find_for_location"
