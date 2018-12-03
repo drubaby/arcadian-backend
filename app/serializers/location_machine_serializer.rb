c3a71b4b33da36f8ebebb5231cfe73465fb0dfac
drubaby <peryton6@gmail.com>
2018-12-03 09:57:25 -0500

    add updated_at for loc_mac serializer

--- app/serializers/location_machine_serializer.rb
+++ app/serializers/location_machine_serializer.rb
@@ -1,5 +1,5 @@
 class LocationMachineSerializer < ActiveModel::Serializer
-  attributes :id, :location_id, :machine_id, :machine, :machine_issues, :is_working
+  attributes :id, :location_id, :machine_id, :machine, :machine_issues, :is_working, :updated_at
   has_one :machine
   has_many :machine_issues
 end
