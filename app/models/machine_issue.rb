73612d54655b0fc8cbfafde16dc7937afe6d4bc1
drubaby <peryton6@gmail.com>
2018-12-04 14:08:20 -0500

    add location name to loc_mac serializer

--- app/models/machine_issue.rb
+++ app/models/machine_issue.rb
@@ -1,4 +1,3 @@
 class MachineIssue < ApplicationRecord
   belongs_to :location_machine
-
 end
--- app/serializers/location_machine_serializer.rb
+++ app/serializers/location_machine_serializer.rb
@@ -1,5 +1,13 @@
 class LocationMachineSerializer < ActiveModel::Serializer
-  attributes :id, :location_id, :machine_id, :machine, :machine_issues, :is_working, :updated_at
+  attributes :id, :location_id, :machine_id, :location_name, :machine, :machine_issues, :is_working, :updated_at
   has_one :machine
   has_many :machine_issues
+
+  def location_name
+    loc_mac = self.object
+    location = Location.find(loc_mac.location_id)
+    location.name
+  end
+
+
 end
