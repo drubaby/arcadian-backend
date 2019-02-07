8c130461f8c8e53215c236e585f656be55571b36
drubaby <peryton6@gmail.com>
2019-02-07 12:41:58 -0500

    rename routes to define /machines remove old references

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :machine_issues, only: [:index, :create, :show, :update, :destroy]
-  resources :location_machines
-  resources :machines, only: [:index, :show]
+  resources :machines
+  resources :machine_types, only: [:index, :show]
   resources :locations, only: [:index, :show, :create]
-  get "/machines_at_location/:id", to: "location_machines#find_for_location"
+  get "/machines_at_location/:id", to: "machines#find_for_location"
   post "/update_location_by_issue", to: "machine_issues#update_location_by_issue"
-  post "update_location_by_machine", to: "location_machines#update_location_by_machine"
+  post "update_location_by_machine", to: "machines#update_location_by_machine"
 
   resources :users, only: [:create]
   post '/login', to: 'auth#create'
